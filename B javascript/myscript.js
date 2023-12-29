d11c9635f1c0dd5fb082659082ea7cd0299660a8
monikaisotiya123 <monikaisotiya123@gmail.com>
2023-12-29 15:00:08 +0530

    i have new file

---
+++ B javascript/myscript.js
@@ -0,0 +1,13 @@
+// ex = 1
+let newelem = document.createElement('p')
+newelem.innerHTML = 'hiii this is a p tag'
+
+let ele = document.getElementById('ele')
+ele.append(newelem)
+
+// ex=2
+
+let newwle = document.createElement('h2')
+newwle.innerHTML = 'ijidj mkmmk mksm mks ks okn k k kq'
+let elem = document.getElementById('moni')
+elem.append(newwle)
